60aaa8a5f81af40eb0b1bd71ecbaa65f41826ee3
Thulasiraj Komminar <>
2019-09-16 14:16:29 +0200

    added to self

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "default" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
   }
 
   egress {
