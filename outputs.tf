3a43b4da77ce4a61721872fb7abe1956d9f7e904
Vinothini Balasubramaniam <vinoammu61@gmail.com>
2023-04-06 15:20:25 +0200

    Export cluster_nodes attribute

--- outputs.tf
+++ outputs.tf
@@ -37,3 +37,8 @@ output "security_group_id" {
   value       = length(aws_security_group.default) > 0 ? aws_security_group.default.id : ""
   description = "The ID of the security group associated with the cluster"
 }
+
+output "cluster_nodes" {
+  value       = aws_redshift_cluster.default.cluster_nodes
+  description = "The nodes in the redshift cluster"
+}
