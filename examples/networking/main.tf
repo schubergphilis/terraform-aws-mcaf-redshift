88c9beba43013fa10038db1e77883537d61840f7
Frank van Boven <frank@cenotaph.nl>
2025-09-30 14:38:50 +0200

    Update Examples

--- examples/networking/main.tf
+++ examples/networking/main.tf
@@ -47,7 +47,7 @@ module "redshift" {
 
 module "vpc" {
   source  = "schubergphilis/mcaf-vpc/aws"
-  version = "~> 1.22.0"
+  version = "~> 3.0.0"
 
   name                = "redshift-vpc"
   availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
